714b722d4ff33202eed6643df2c96b9323be9647
paring <paring@pikokr.dev>
2024-01-18 03:28:49 +0900

    feat: seo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,24 +1,28 @@
-import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import React from "react"
+import { DocsThemeConfig } from "nextra-theme-docs"
 
 const config: DocsThemeConfig = {
-  logo: <b>Command.TS</b>,
-  project: {
-    link: "https://github.com/pikokr/command.ts",
-  },
-  chat: {
-    link: "https://discord.gg/EkFHRG4TZZ",
-  },
-  docsRepositoryBase: "https://github.com/pikokr/command.ts-docs/edit/dev",
-  footer: {
-    text: "Command.TS",
-  },
-  search: {
-    placeholder: "문서 검색하기...",
-  },
-  sidebar: {
-    toggleButton: true,
-  },
-};
+	logo: <b>Command.TS</b>,
+	project: {
+		link: "https://github.com/pikokr/command.ts",
+	},
+	chat: {
+		link: "https://discord.gg/EkFHRG4TZZ",
+	},
+	docsRepositoryBase: "https://github.com/pikokr/command.ts-docs/edit/dev",
+	footer: {
+		text: "Command.TS",
+	},
+	search: {
+		placeholder: "문서 검색하기...",
+	},
+	sidebar: {
+		toggleButton: true,
+	},
+	useNextSeoProps: () => ({
+		titleTemplate: "%s - Command.TS",
+		description: "A simple framework for discord.js typescript",
+	}),
+}
 
-export default config;
+export default config
